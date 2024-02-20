4258aba0ec98fa728576c2fe96a1e6b9c095bdcc
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-02-20 19:40:05 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,130 @@
 import { useState } from "react";
-import { Statistics } from "./Statistics.jsx";
-import { Button } from "./Button.jsx";
+import { Anecdote, AnecdoteCard } from "./anecdotes";
+import { anecdotes } from "./anecdotes/data";
+import { getMostVotedAnecdoteData } from "./anecdotes/helpers";
+import { initialVoteState } from "./anecdotes/state/initialVoteState";
+import { Statistics } from "./feedback";
+import { statisticsData } from "./feedback/data";
+import {
+  calculatePositiveAverage,
+  calculateTotalAverage,
+  shouldRenderStatistics,
+} from "./feedback/helpers";
+import { Button, ButtonSet, MainHeader, SectionHeader } from "./shared";
 
-// Definició del component App
 const App = () => {
-  // Definició dels estats inicials
+  // feedback state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [selectedAnecdote, setAnecdote] = useState(0);
-  const anecdotes = [
-    // Array que conté diferents anècdotes
-    // (Per brevetat, només s'han inclòs algunes)
+
+  // anecdotes state
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState(initialVoteState);
+
+  // feedback handlers
+  const totalFeedback = good + neutral + bad;
+
+  const handleIncreaseGood = () => {
+    setGood((prevTotal) => prevTotal + 1);
+  };
+
+  const handleIncreaseNeutral = () => {
+    setNeutral((prevTotal) => prevTotal + 1);
+  };
+
+  const handleIncreaseBad = () => {
+    setBad((prevTotal) => prevTotal + 1);
+  };
+
+  // feedback data
+  const averageStatistic = calculateTotalAverage({ good, bad, totalFeedback });
+  const positiveAverage = calculatePositiveAverage({ good, totalFeedback });
+
+  const statistics = [
+    statisticsData.goodStatistic(good),
+    statisticsData.neutralStatistic(neutral),
+    statisticsData.badStatistic(bad),
+    statisticsData.allStatistic(totalFeedback),
+    statisticsData.totalAverageStatistic(averageStatistic),
+    statisticsData.positiveAverageStatistic(positiveAverage),
   ];
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-  const [mostVotes, setMostVotes] = useState(0);
 
-  // Funcions per incrementar el comptador de feedbacks positius, neutres i negatius
-  const handleIncrementGood = () => {
-    setGood((prevGood) => prevGood + 1);
-  };
-  const handleIncrementNeutral = () => {
-    setNeutral((prevNeutral) => prevNeutral + 1);
-  };
-  const handleIncrementBad = () => {
-    setBad((prevBad) => prevBad + 1);
+  // anecdotes handlers
+  const handleRandomAnecdote = () => {
+    const anecdotesRange = anecdotes.length;
+    const index = Math.floor(Math.random() * anecdotesRange);
+
+    setSelected(index);
   };
 
-  // Funció per seleccionar una anècdota aleatòria
-  const handleRandom = () => {
-    setAnecdote(() => Math.floor(Math.random() * anecdotes.length));
+  const handleUpdateVotes = () => {
+    setVoted((prevVotes) => {
+      const nextVotes = [...prevVotes];
+      nextVotes[selected] += 1;
+
+      return nextVotes;
+    });
   };
 
-  // Funció per votar una anècdota
-  const handleVote = () => {
-    const updatedVotes = votes.map((vote, index) =>
-      index === selectedAnecdote ? vote + 1 : vote
-    );
-    setVotes(updatedVotes);
-  };
+  // anecdotes data
+  const { mostVotedAnecdote, maxVoteValue } = getMostVotedAnecdoteData({
+    votesCollection: voted,
+    anecdotesCollection: anecdotes,
+  });
 
-  // Funció per trobar l'anècdota amb més vots
-  const handleMostVotes = () => {
-    let mostVoted = 0;
-    for (let index = 0; index < votes.length; index++) {
-      if (votes[index] > votes[mostVoted]) {
-        mostVoted = index;
-      }
-    }
-    setMostVotes(mostVoted);
-  };
-
-  // Càlcul del total de feedbacks i el resultat acumulat
-  let total = good + neutral + bad;
-  let result = good - bad;
-
-  // Renderització del component
   return (
-    <div>
-      <div>
-        {/* Secció per a deixar el feedback */}
-        <h1>donar feedback</h1>
-        <Button callback={handleIncrementGood} text={"positiu"} />
-        <Button callback={handleIncrementNeutral} text={"neutre"} />
-        <Button callback={handleIncrementBad} text={"negatiu"} />
-
-        {/* Secció d'estadístiques */}
-        <h1>estadístiques</h1>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          result={result}
-        />
-      </div>
-
-      <div>
-        {/* Secció per a mostrar una anècdota aleatòria */}
-        <h1>Anècdota del dia</h1>
-        <p>{anecdotes[selectedAnecdote]}</p>
-        <p>te {votes[selectedAnecdote]} vots</p>
-        <button onClick={() => {
-            handleVote();
-            handleMostVotes();
-          }}>
-          votar
-        </button>
-        <button onClick={handleRandom}>següent anècdota</button>
-      </div>
-
-      <div>
-        {/* Secció per a mostrar l'anècdota amb més vots */}
-        <h1>Anècdota amb més vots</h1>
-        <p>{anecdotes[mostVotes]}</p>
-        <p>te {votes[mostVotes]} vots</p>
-      </div>
-    </div>
+    <>
+      <MainHeader text="React state management" />
+      <main className="main">
+        <section className="section">
+          <SectionHeader text="give feedback" />
+          <ButtonSet>
+            <Button
+              label="good"
+              handleUpdate={handleIncreaseGood}
+              level="success"
+            />
+            <Button
+              label="neutral"
+              handleUpdate={handleIncreaseNeutral}
+              level="secondary"
+            />
+            <Button
+              label="bad"
+              handleUpdate={handleIncreaseBad}
+              level="danger"
+            />
+          </ButtonSet>
+          <Statistics
+            statistics={statistics}
+            shouldRenderStatistics={shouldRenderStatistics({ totalFeedback })}
+          />
+        </section>
+        <section className="section">
+          <SectionHeader text="anecdotes" />
+          <AnecdoteCard title="Anecdote of the day">
+            <Anecdote content={anecdotes[selected]} votes={voted[selected]} />
+            <ButtonSet>
+              <Button
+                label="Vote"
+                handleUpdate={handleUpdateVotes}
+                level="success"
+              />
+              <Button
+                label="Next"
+                handleUpdate={handleRandomAnecdote}
+                level="secondary"
+              />
+            </ButtonSet>
+          </AnecdoteCard>
+          <AnecdoteCard title="Anecdote with most votes">
+            <Anecdote content={mostVotedAnecdote} votes={maxVoteValue} />
+          </AnecdoteCard>
+        </section>
+      </main>
+    </>
   );
 };
 
-export default App; // Exportació del component App
+export default App;
