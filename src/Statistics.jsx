7312f50c73a734bf42893d3acf939885499c479c
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-02-20 18:54:56 +0100

    no comments

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -8,12 +8,12 @@ export const Statistics = ({ good, neutral, bad, total, result }) => {
   return (
     <table>
       <tbody>
-        <StatisticLine text="good" value={good} /> // Mostra el nombre de feedbacks positius
-        <StatisticLine text="neutral" value={neutral} /> // Mostra el nombre de feedbacks neutres
-        <StatisticLine text="bad" value={bad} /> // Mostra el nombre de feedbacks negatius
-        <StatisticLine text="all" value={total} /> // Mostra el total de feedbacks rebuts
-        <StatisticLine text="average" value={result / total} /> // Mostra la mitjana dels feedbacks
-        <StatisticLine text="positive" value={`${(good / total) * 100} %`} /> // Mostra el percentatge de feedbacks positius
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={result / total} />
+        <StatisticLine text="positive" value={`${(good / total) * 100} %`} />
       </tbody>
     </table>
   );
